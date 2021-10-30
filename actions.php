3fad122bf3db00d9045f13caf842b4bbc9cfc347
Ali Tokmakcı <46011130+alitokmakci@users.noreply.github.com>
2021-10-30 19:49:33 +0300

    db bug fixed

--- actions.php
+++ actions.php
@@ -1,24 +1,30 @@
 <?php
-	if (isset($_POST['submit'])) {
-		$task = $_POST['task'];
-		if (empty($task)) {
-			$_SESSION['error'] = "Burayı doldurmalısın.";
-		}else {
-			if (isset($_SESSION['error'])) {
-				unset($_SESSION['error']);
-			}
-			mysqli_query($db, "INSERT INTO tasks (task) VALUES ('$task')");
-			$_SESSION['success'] = 'Görev Başarıyla Eklendi!';
-			header('location: index.php');
+
+require_once('./db.php');
+/**
+* Adding new task to the database
+*/
+if (isset($_POST['submit'])) {
+	$task = $_POST['task'];
+	if (empty($task)) {
+		$_SESSION['error'] = "Burayı doldurmalısın.";
+	}else {
+		if (isset($_SESSION['error'])) {
+			unset($_SESSION['error']);
 		}
+		mysqli_query($db, "INSERT INTO tasks (task) VALUES ('$task')");
+		$_SESSION['success'] = 'Görev Başarıyla Eklendi!';
+		header('Location: index.php');
 	}
+}
 
-	// Görev Silme
-
-	if (isset($_GET['del_task'])) {
-		$id = $_GET['del_task'];
-		mysqli_query($db, "DELETE FROM tasks WHERE id=$id");
-		header('location: index.php');
-	}
+/**
+* Removing a existing task from database
+*/
+if (isset($_GET['del_task'])) {
+	$id = $_GET['del_task'];
+	mysqli_query($db, "DELETE FROM tasks WHERE id=$id");
+	header('Location: index.php');
+}
  ?>
 
